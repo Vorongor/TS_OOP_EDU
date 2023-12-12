e57845de80be9fc613883ef1a3e8871707794d22
Vorongor <mr.maddarknes@gmail.com>
2023-12-12 16:44:25 +0200

    hw3

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,58 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  protected key: Key;
+  tenants: Person[];
+
+  constructor(key: Key) {
+    this.door = false;
+    this.key = key;
+    this.tenants = [];
+  }
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Person has entered the house.");
+    } else {
+      console.log("Door is closed. Cannot enter.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door opened successfully.");
+    } else {
+      console.log("Wrong key. Door remains closed.");
+    }
+  }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +61,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
